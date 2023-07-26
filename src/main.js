a0c73d1ef779ec5085620d0f8342a4c790c76b19
Eralpozcan <eralpozcans@gmail.com>
2023-07-26 16:44:36 +0300

    feat: sentry env is priv

--- src/main.js
+++ src/main.js
@@ -44,10 +44,10 @@ Sentry.init({
     new Sentry.Replay(),
   ],
   // Performance Monitoring
-  tracesSampleRate: 0.2, // Capture 100% of the transactions, reduce in production!
+  tracesSampleRate: import.meta.env.VITE_VUE_APP_SENTRY_TRACE_SAMPLE_RATE, // Capture 100% of the transactions, reduce in production!
   // Session Replay
-  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 0.5, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  replaysSessionSampleRate: import.meta.env.VITE_VUE_APP_SENTRY_SESSION_SAMPLE_RATE, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: import.meta.env.VITE_VUE_APP_SENTRY_ON_ERROR_SAMPLE_RATE, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
   trackComponents: true,
 });
 inject();
